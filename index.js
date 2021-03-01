f650747c5dc609136ddf6a83b5ed74558e28b8cb
YeFei <316606233@qq.com>
2021-03-01 13:52:23 +0800

    bugfix: ApiFail

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const debug = require('debug')('zenweb:api');
-const { ApiFail } = require('./lib/api_fail');
+
+class ApiFail extends Error {
+  /**
+   * @param {string} [message] 错误消息
+   * @param {number} [code] 错误代码
+   * @param {*} [data] 附加数据
+   * @param {number} [httpCode] 错误代码
+   */
+  constructor(message, code, data, httpCode) {
+    super(message);
+    this.expose = true;
+    this.code = code;
+    this.data = data;
+    this.httpCode = httpCode || 422;
+  }
+}
 
 /**
  * 安装拦截 fail 错误
